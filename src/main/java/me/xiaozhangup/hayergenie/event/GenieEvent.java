8328b54c00c86e15a51c1df70350cc6c19850afe
xiaozhangup <68374769+xiaozhangup@users.noreply.github.com>
2022-10-27 14:37:25 +0800

    改文本

--- src/main/java/me/xiaozhangup/hayergenie/event/GenieEvent.java
+++ src/main/java/me/xiaozhangup/hayergenie/event/GenieEvent.java
@@ -24,7 +24,7 @@ public class GenieEvent implements Listener {
 
     public static ItemStack spawn = Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMTI4OWQ1YjE3ODYyNmVhMjNkMGIwYzNkMmRmNWMwODVlODM3NTA1NmJmNjg1YjVlZDViYjQ3N2ZlODQ3MmQ5NCJ9fX0=", "&x&c&e&d&e&f&e返回出生点 &7(主城)", "", "&7在游戏初期,你应当", "&7多在主城游览");
     public static ItemStack tree = Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjc2YmQ5Y2M1NWI2YTI3NWIwYjZmYzc1NDk0NmE3NDgyM2ZhMmZlOTZiNzMyYzRkZDllMTk0NTE4MmZiYTUifX19", "&x&a&f&a&6&9&e前往树场", "", "&7你可以在这里获取", "&7大量矿物和树木");
-    public static ItemStack quest = Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjg2ZGYyNWE5ZDg1ZTc1NzU3ZjA0YjdkNzllYzc2MDc2YmY5NWRhZGE2OGYyY2YyYWY0OTRmOWY4OGZmMTg3MCJ9fX0=", "&x&f&7&e&3&a&4完成岛屿任务", "", "&7获取岛屿水晶和货币");
+    public static ItemStack quest = Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZjg2ZGYyNWE5ZDg1ZTc1NzU3ZjA0YjdkNzllYzc2MDc2YmY5NWRhZGE2OGYyY2YyYWY0OTRmOWY4OGZmMTg3MCJ9fX0=", "&x&f&7&e&3&a&4完成岛屿任务", "", "&7以此获取岛屿水晶和货币", "&e并且这是升级岛屿等级的", "&e唯一方法!");
     public static ItemStack main = Skull.getSkull("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNjE5M2IyNGQ3Y2IwZjg2M2I5ODdhNTRiMDI2YmQ5MDE4ZmFjMTU2NzFiM2ZiYmVjYzdkNmUyNTNjMjU3YiJ9fX0=", "&x&f&f&f&2&d&f打开主菜单", "", "&7打开服务器主菜单");
     public static ItemStack push = IBuilder.buildItem(Material.BARRIER, "&c从这里移除 Allay");
 
@@ -36,13 +36,13 @@ public class GenieEvent implements Listener {
                 if (GenieMaster.isOnLand(player) && GenieMaster.getGenieID(GenieMaster.landFromPlayer(player)) == null) {
                     GenieMaster.creatGenie(e.getClickedBlock().getLocation(), player);
                     e.setCancelled(true);
-                    player.sendMessage(IString.addColor("    "));
+                    player.sendMessage(IString.addColor(" \n \n \n \n "));
                     player.sendMessage(IString.addColor("    &f你好! 我是你的岛屿精灵 &bAllay&f!"));
                     player.sendMessage(IString.addColor("    &7我会在你岛屿的工作台上生成.当然,拆除工作台后我不会消失"));
                     player.sendMessage(IString.addColor("    &7右键我可以得到一些很基础的帮助内容."));
                     player.sendMessage(IString.addColor("    "));
                     player.sendMessage(IString.addColor("    &e当你岛屿没有Allay时,右键任意工作台将召唤Allay!"));
-                    player.sendMessage(IString.addColor("    "));
+                    player.sendMessage(IString.addColor("  \n  "));
                 }
             }
         }
